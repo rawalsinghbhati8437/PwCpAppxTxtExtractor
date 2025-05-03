6472d143aa2b61a0d2e876a43f75ffff3ebfaceb
rawalsinghbhati8437 <rawalsinghbhati8437@gmail.com>
2025-05-03 20:12:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "24473318"
-api_hash = "e7dd0576c5ac0ff8f90971d6bb04c8f5"
-bot_token = "8034069514:AAHUBpzSCq41jPwsJkDbXuEoVC_yCxzyuw0"
-auth_users = [5840594311]
+api_id = "16494736"
+api_hash = "2cb7fa5859e2de684e3e10d9c049621a"
+bot_token = "7615391379:AAGzD8NvMWRNPnaXD7xLjWt25zAleK5TjnM"
+auth_users = [6214273135]
